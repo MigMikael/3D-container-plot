6b58809b4f57c5f10751d4a4193c2c7099801b37
Chanachai Puttaruksa <chanachai_mig@hotmail.com>
2020-10-01 20:56:14 +0700

    random bin and item for test

--- test_packer.py
+++ test_packer.py
@@ -1,26 +1,69 @@
 from py3dbp import Packer, Bin, Item
 from plotter import plot_multiple_package
 import json
+import random
+import string
+
+
+def get_random_string(length):
+    letters = string.ascii_lowercase
+    result_str = ''.join(random.choice(letters) for i in range(length))
+    return result_str
+
+
+def random_bin():
+    name_len = random.randrange(5, 10)
+    bin_name = get_random_string(name_len)
+    
+    w = round(random.uniform(0.0, 1000.0), 2)
+    h = round(random.uniform(0.0, 1000.0), 2)
+    d = round(random.uniform(0.0, 1000.0), 2)
+    mw = round(random.uniform(0.0, 100000.0), 2)
+    new_bin = Bin(bin_name, w, h, d, mw)
+    print(new_bin.string())
+    return new_bin
+
+
+def random_item():
+    name_len = random.randrange(3, 8)
+    item_name = get_random_string(name_len)
+    
+    w = round(random.uniform(0.0, 200.0), 2)
+    h = round(random.uniform(0.0, 200.0), 2)
+    d = round(random.uniform(0.0, 200.0), 2)
+    mw = round(random.uniform(0.0, 5000.0), 2)
+    new_item = Item(item_name, w, h, d, mw)
+    print(new_item.string())
+    return new_item
+
 
 packer = Packer()
 
+total_bin = random.randrange(5, 10)
+for i in range(total_bin):
+    packer.add_bin(random_bin())
+
+total_item = random.randrange(10, 15)
+for i in range(total_item):
+    packer.add_item(random_item())
+
 ''' Test 1 From example '''
-packer.add_bin(Bin('small-envelope', 11.5, 6.125, 0.25, 10))
-packer.add_bin(Bin('large-envelope', 15.0, 12.0, 0.75, 15))
-packer.add_bin(Bin('small-box', 8.625, 5.375, 1.625, 70.0))
-packer.add_bin(Bin('medium-box', 11.0, 8.5, 5.5, 70.0))
-packer.add_bin(Bin('medium-2-box', 13.625, 11.875, 3.375, 70.0))
-packer.add_bin(Bin('large-box', 12.0, 12.0, 5.5, 70.0))
-packer.add_bin(Bin('large-2-box', 23.6875, 11.75, 3.0, 70.0))
-packer.add_item(Item('50g [powder 1]', 3.9370, 1.9685, 1.9685, 1))
-packer.add_item(Item('50g [powder 2]', 3.9370, 1.9685, 1.9685, 2))
-packer.add_item(Item('50g [powder 3]', 3.9370, 1.9685, 1.9685, 3))
-packer.add_item(Item('250g [powder 4]', 7.8740, 3.9370, 1.9685, 4))
-packer.add_item(Item('250g [powder 5]', 7.8740, 3.9370, 1.9685, 5))
-packer.add_item(Item('250g [powder 6]', 7.8740, 3.9370, 1.9685, 6))
-packer.add_item(Item('250g [powder 7]', 7.8740, 3.9370, 1.9685, 7))
-packer.add_item(Item('250g [powder 8]', 7.8740, 3.9370, 1.9685, 8))
-packer.add_item(Item('250g [powder 9]', 7.8740, 3.9370, 1.9685, 9))
+# packer.add_bin(Bin('small-envelope', 11.5, 6.125, 0.25, 10))
+# packer.add_bin(Bin('large-envelope', 15.0, 12.0, 0.75, 15))
+# packer.add_bin(Bin('small-box', 8.625, 5.375, 1.625, 70.0))
+# packer.add_bin(Bin('medium-box', 11.0, 8.5, 5.5, 70.0))
+# packer.add_bin(Bin('medium-2-box', 13.625, 11.875, 3.375, 70.0))
+# packer.add_bin(Bin('large-box', 12.0, 12.0, 5.5, 70.0))
+# packer.add_bin(Bin('large-2-box', 23.6875, 11.75, 3.0, 70.0))
+# packer.add_item(Item('50g [powder 1]', 3.9370, 1.9685, 1.9685, 1))
+# packer.add_item(Item('50g [powder 2]', 3.9370, 1.9685, 1.9685, 2))
+# packer.add_item(Item('50g [powder 3]', 3.9370, 1.9685, 1.9685, 3))
+# packer.add_item(Item('250g [powder 4]', 7.8740, 3.9370, 1.9685, 4))
+# packer.add_item(Item('250g [powder 5]', 7.8740, 3.9370, 1.9685, 5))
+# packer.add_item(Item('250g [powder 6]', 7.8740, 3.9370, 1.9685, 6))
+# packer.add_item(Item('250g [powder 7]', 7.8740, 3.9370, 1.9685, 7))
+# packer.add_item(Item('250g [powder 8]', 7.8740, 3.9370, 1.9685, 8))
+# packer.add_item(Item('250g [powder 9]', 7.8740, 3.9370, 1.9685, 9))
 
 
 ''' Test 2 Edge case that needs rotation '''
@@ -55,22 +98,7 @@ packer.add_item(Item('250g [powder 9]', 7.8740, 3.9370, 1.9685, 9))
 # packer.add_item(Item("Item 7", 100, 100, 30, 10))
 
 
-# Try more 
-
-
 packer.pack(distribute_items=True)
-    
-
-# for b in packer.bins:
-#     print(":::::::::::", b.string())
-#     print("FITTED ITEMS:")
-#     for item in b.items:
-#         print("====> ", item.string())
-#     print("UNFITTED ITEMS:")
-#     for item in b.unfitted_items:
-#         print("====> ", item.string())
-#     print("***************************************************")
-#     print("***************************************************")
 
 
 figure_num = 0
@@ -83,9 +111,11 @@ for b in packer.bins:
     result_dict["height"] = float(b.height)
     result_dict["length"] = float(b.depth)
     result_dict["volume"] = float(b.width * b.height * b.depth)
-    
     result_dict["placement"] = []
+
+    print("FITTED ITEMS:")
     for item in b.items:
+        print("====> ", item.string())
         item_dict = {}
         item_dict["name"] = item.name
         item_dict["x"] = float(item.position[0])
@@ -98,22 +128,18 @@ for b in packer.bins:
         item_dict["length"] = float(d)
         result_dict["placement"].append(item_dict)
 
-        print("====> ", item.string())
-        print("Default Dimen", item.width, item.height, item.depth)
-        print("GET Dimension", item.get_dimension())
-        print("Rotation Type", item.rotation_type)
-        print()
-
-    result_json = json.dumps(result_dict)
-    print(result_json)
-    print("\n\n")
-
     print("UNFITTED ITEMS:")
     for item in b.unfitted_items:
         print("====> ", item.string())
 
-    
     if len(b.items) > 0:
+        print()
+        result_json = json.dumps(result_dict)
+        print(result_json)
+
         plt1 = plot_multiple_package(result_json, b.name, figure_num)
         plt1.show()
         figure_num+=1
+    
+    print("\n\n")
+
